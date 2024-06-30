64ca0433b2a7647720109d8d83bc851b25a6e818
Krzysztof Bistram <bistram.k@gmail.com>
2024-06-30 18:40:31 +0200

    Automapper Config

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using dotnet_jumpstart.DTOs.Character;
+
+namespace dotnet_jumpstart
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
